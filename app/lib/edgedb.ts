373e114fa8c0690ae19c236ddb45d1bc67eaeb1d
Dijana Pavlovic <dijana@edgedb.com>
2024-07-02 10:16:34 +0200

    Update baseUrl auth config

--- app/lib/edgedb.ts
+++ app/lib/edgedb.ts
@@ -4,6 +4,6 @@ import createAuth from "@edgedb/auth-nextjs/app";
 export const client = createClient();
 
 export const auth = createAuth(client, {
-  baseUrl: "http://localhost:3000",
+  baseUrl: process.env.BASE_URL || "http://localhost:3000",
   authRoutesPath: "api/auth",
 });
