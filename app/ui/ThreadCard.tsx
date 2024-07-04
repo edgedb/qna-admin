8937356afaa76954e0facad8c51967a3b2647e29
Dijana Pavlovic <dijana@edgedb.com>
2024-07-04 10:13:22 +0200

    Fix ul styles & threads navigation

--- app/ui/ThreadCard.tsx
+++ app/ui/ThreadCard.tsx
@@ -5,15 +5,21 @@ import { usePathname } from "next/navigation";
 import { ArrowRightIcon } from "./icons";
 import { ThreadEssential } from "../lib/db/threads";
 
-export default function ThreadCard({ thread }: { thread: ThreadEssential }) {
+export default function ThreadCard({
+  thread,
+  page,
+}: {
+  thread: ThreadEssential;
+  page?: number;
+}) {
   const pathname = usePathname();
   const segment = pathname.split("/").pop();
-
+  const pageParam = page ? `?page=${page}` : "";
   const { id, messages } = thread;
 
   return (
     <Link
-      href={`/threads/${id}`}
+      href={`/threads/${id}${pageParam}`}
       className={`rounded flex py-3 pl-3 pr-1.5 mb-4 items-center hover:bg-activeElement transition-colors ${
         segment === id ? `bg-activeElement` : "bg-element"
       }`}
