309c29f8041461b66f5018572b1c5f1b057e7e59
Dijana Pavlovic <dijana@edgedb.com>
2024-08-01 16:23:41 +0200

    Lazy creation of Discord bot

--- app/api/interactions/route.ts
+++ app/api/interactions/route.ts
@@ -2,13 +2,9 @@ import {
   InteractionResponseType,
   InteractionType,
 } from "discord-api-types/v10";
-import { Bot } from "@/app/lib/discord/bot";
+import { getBot } from "@/app/lib/discord/getBot";
 import { getEnvironment } from "../../../envs";
-import createClient from "edgedb";
 import { verifyInteractionRequest } from "@/app/lib/discord/verify-interaction-request";
-
-const bot = new Bot(createClient(), getEnvironment().discordToken);
-await bot.initialize();
 
 export async function POST(req: Request) {
   const verifyResult = await verifyInteractionRequest(
@@ -29,7 +25,8 @@ export async function POST(req: Request) {
   }
 
   try {
-    const result = await bot.processInteraction(interaction);
+    const bot = await getBot();
+    const result = await bot?.processInteraction(interaction);
 
     return Response.json(result);
   } catch (err: any) {
