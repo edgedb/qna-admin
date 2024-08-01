1d4fc0d070bd90d882ed0c8f61a4f8dfaf3f8cb0
Dijana Pavlovic <dijana@edgedb.com>
2024-08-01 14:12:13 +0200

    Add back envs.s file

---
+++ app/envs.ts
@@ -0,0 +1,30 @@
+if (!process.env.DISCORD_CLIENT_ID) {
+  throw new Error("DISCORD_CLIENT_ID env var not configured");
+}
+export const discordClientId = process.env.DISCORD_CLIENT_ID;
+
+if (!process.env.DISCORD_CLIENT_PUBLIC_KEY) {
+  throw new Error("DISCORD_CLIENT_PUBLIC_KEY env var not configured");
+}
+export const discordClientPublicKey = process.env.DISCORD_CLIENT_PUBLIC_KEY;
+
+if (!process.env.DISCORD_TOKEN) {
+  throw new Error(`DISCORD_TOKEN env var not configured`);
+}
+export const discordToken = process.env.DISCORD_TOKEN;
+
+if (!process.env.DISCORD_GUILD_ID) {
+  throw new Error("DISCORD_GUILD_ID env var not configured");
+}
+export const discordGuildId = process.env.DISCORD_GUILD_ID;
+
+if (!process.env.DISCORD_MODERATION_ACCESS_ROLES) {
+  throw new Error("DISCORD_MODERATION_ACCESS_ROLES env var not configured");
+}
+export const authorizedRoleIds =
+  process.env.DISCORD_MODERATION_ACCESS_ROLES.split(",");
+
+if (!process.env.REVIEW_CHANNEL_ID) {
+  throw new Error("REVIEW_CHANNEL_ID env var not configured");
+}
+export const reviewChannelId = process.env.REVIEW_CHANNEL_ID;
